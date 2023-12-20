5d41a47e25e948096dde411740d8aa8e845372c5
anasbag03 <anas.baghdadi@student.ehb.be>
2023-12-20 01:08:55 +0100

    login

--- login.js
+++ login.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
 const { MongoClient } = require('mongodb');
+const cookieParser = require('cookie-parser');
+const { v4: uuidv4, validate: uuidValidate } = require('uuid');
 
 const client = new MongoClient("mongodb+srv://login:test123@cluster0.ayowtk8.mongodb.net/?retryWrites=true&w=majority");
 
 router.use(express.urlencoded({ extended: false }));
+router.use(cors());
+router.use(express.json());
+router.use(cookieParser());
 
 router.get("/testMongo", async (req, res) => {
     try {
@@ -21,6 +27,148 @@ router.get("/testMongo", async (req, res) => {
     } finally {
         await client.close();
     }
+});
+
+router.post("/register", async (req, res) => {
+    if (!req.body.username || !req.body.email || !req.body.password) {
+        res.status(400).send({
+            status: "Bad Request",
+            message: "Some fields are missing: username, email, password"
+        });
+        return;
+    }
+
+    try {
+        await client.connect();
+        const user = {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            uuid: uuidv4()
+        };
+        const colli = client.db('logintutorial').collection('users');
+        const insertedUser = await colli.insertOne(user);
+        res.status(201).send({
+            status: "Saved",
+            message: "User has been saved!",
+            data: insertedUser
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            error: 'Something went wrong!',
+            value: error
+        });
+    } finally {
+        await client.close();
+    }
+});
+
+router.post("/login", async (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        res.status(400).send({
+            status: "Bad Request",
+            message: "Some fields are missing: email, password"
+        });
+        return;
+    }
+
+    try {
+        await client.connect();
+        const loginuser = {
+            email: req.body.email,
+            password: req.body.password,
+        };
+        const colli = client.db('logintutorial').collection('users');
+        const query = { email: loginuser.email };
+        const user = await colli.findOne(query);
+
+        if (user) {
+            if (user.password == loginuser.password) {
+                res.status(200).send({
+                    status: "Authentication successful!",
+                    message: "You are logged in!",
+                    data: {
+                        username: user.username,
+                        email: user.email,
+                        uuid: user.uuid,
+                    }
+                });
+            } else {
+                res.status(401).send({
+                    status: "Authentication error",
+                    message: "Password is incorrect!"
+                });
+            }
+        } else {
+            res.status(401).send({
+                status: "Authentication error",
+                message: "No user with this email has been found! Make sure you register first."
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            error: 'Something went wrong!',
+            value: error
+        });
+    } finally {
+        await client.close();
+    }
+});
+
+router.post("/verifyID", async (req, res) => {
+    if (!req.body.uuid) {
+        res.status(400).send({
+            status: "Bad Request",
+            message: "ID is missing"
+        });
+        return;
+    } else {
+        if (!uuidValidate(req.body.uuid)) {
+            res.status(400).send({
+                status: "Bad Request",
+                message: "ID is not a valid UUID"
+            });
+            return;
+        }
+    }
+    try {
+        await client.connect();
+        const colli = client.db('logintutorial').collection('users');
+        const query = { uuid: req.body.uuid };
+        const user = await colli.findOne(query);
+
+        if (user) {
+            res.status(200).send({
+                status: "Verified",
+                message: "Your UUID is valid.",
+                data: {
+                    username: user.username,
+                    email: user.email,
+                    uuid: user.uuid,
+                }
+            });
+        } else {
+            res.status(401).send({
+                status: "Verify error",
+                message: `No user exists with uuid ${req.body.uuid}`
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            error: 'Something went wrong!',
+            value: error
+        });
+    } finally {
+        await client.close();
+    }
+});
+
+router.post('/logout', (req, res) => {
+    res.clearCookie('session');
+    res.send('Logout successful');
 });
 
 module.exports = router;
